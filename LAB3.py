960b5ed164711f825a6b2a4e74a24c2fd82695f0
Turki <turki_salam@hotmail.com>
2024-09-15 20:31:17 +0300

    I added "Create Group Method"

--- LAB3.py
+++ LAB3.py
@@ -2,3 +2,22 @@ def readStudents(stdList):
     
     for student in stdList:
         print(student + "  ")
+
+
+def createGroup(stdList, sizeOfGroups = 2):
+    
+    numberOfGroups = round((len(stdList) / sizeOfGroups))
+
+    for k in range(numberOfGroups):
+
+        print(f"Group ({k + 1}): ")
+
+        for i in range(1,3):
+            if(len(stdList) > 0):
+                print(f"{i}. {stdList.pop()}") 
+
+
+
+listTem = ["A", "B", "C", "D"]
+
+createGroup(listTem)
