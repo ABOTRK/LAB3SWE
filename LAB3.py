56c6b7e161eea8979603e4f81246f3595753fcb5
Turki <turki_salam@hotmail.com>
2024-09-15 15:43:45 +0300

    I add a method to read studentsList and print each one

--- LAB3.py
+++ LAB3.py
@@ -1,11 +1,4 @@
-def add(num1, num2):
-    return num1 + num2
-
-def divide(num1, num2):
-    return num1 / num2
-
-def sub():
-    return 1
-
-print("Welcome There!")
-print(f"Sum = {add(1,2)}")
+def readStudents(stdList):
+    
+    for student in stdList:
+        print(student + "  ")
